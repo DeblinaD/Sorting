1a451a013e1e20073c939f10984737b319a96279
Deblina Das <77379919+DeblinaD@users.noreply.github.com>
2021-07-24 19:54:56 +0530

    corrected mistakes

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,26 +1,41 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
+ 
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+// A function to implement bubble sort
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n-1; i++)    
+     
+    // Last i elements are already in place
+    for (j = 0; j < n-i-1; j++)
+        if (arr[j] > arr[j+1])
+            swap(&arr[j], &arr[j+1]);
+}
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+ 
+// Driver code
 int main()
 {
-    int array[7]={4,9,6,1,8,3,2}, i, count;
-
-    for(int j=0; j<7; j++) // this loop is for going through the array
-    {
-        for(i=0; i<7; i++)  // this loop is for swaping
-        {   
-             if(array[i]>array[i+1])
-                 {
-                     count=array[i]; //storing the value of array[i] in count so that we can shift to next.
-                     array[i]=array[i+1];// here the i value is shifted to the next.
-                    array[i+1]=count;
-                 }
-
-        }
-    }
-    cout<<"required sorted array is: "<<endl;
-    for(i=0; i<7; i++)
-    cout<<array[i]<<" "<<endl;
-
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    cout<<"Sorted array: \n";
+    printArray(arr, n);
     return 0;
 }
