70f7ae47930a9a45f5fa468df2687eac096f6e6c
Deblina Das <77379919+DeblinaD@users.noreply.github.com>
2021-12-29 22:48:12 +0530

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -14,9 +14,13 @@ void bubbleSort(int arr[], int n)
     int i, j;
     for (i = 0; i < n-1; i++)    
     // Last i elements are already gets in the last position after first pass therefore we continue the loop fpr n-1 times.
-    for (j = 0; j < n-i-1; j++)
-        if (arr[j] > arr[j+1])
+    {
+     for (j = 0; j < n-i-1; j++)
+     {
+       if (arr[j] > arr[j+1])
             swap(&arr[j], &arr[j+1]);
+     }
+    }
 }
  
 /* Function to print an array */
